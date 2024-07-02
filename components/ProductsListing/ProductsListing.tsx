5da81ca8085714f68a6b53e37349cf2d42af5471
Andrew Sayward <andrewsayward@hotmail.co.uk>
2024-07-02 18:53:27 +0100

    add sorting functionality

--- components/ProductsListing/ProductsListing.tsx
+++ components/ProductsListing/ProductsListing.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Product from '@/models/Product';
 import Image from 'next/image';
 import styles from './products-listing.module.scss';
@@ -7,25 +8,54 @@ type Props = {
   products: Product[];
 };
 
-const ProductsListing = (props: Props) => {
+const ProductsListing = ({ products }: Props) => {
+  const [sortedProducts, setSortedProducts] = useState(products);
+
+  const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const order = e.target.value;
+
+    const sorted = [...products].sort((a, b) => {
+      if (order === 'ascending') {
+        //We compare the values being sorted by subtracting them from each other and comparing the negative and positive values
+        return a.id - b.id;
+      } else if (order === 'descending') {
+        return b.id - a.id;
+      }
+      return 0;
+    });
+
+    setSortedProducts(sorted);
+  };
+
   return (
-    <div className={styles.productListingContainer}>
-      {props.products.map((product) => {
-        return (
-          <div key={product.id} className={styles.productListing}>
-            <span>{'£' + product.price}</span>
-            <h3>{product.title}</h3>
-            <div className={styles.image}>
-              <Image src={product.image} alt={product.title} fill />
+    <section>
+      <div className={styles.sort}>
+        <select onChange={handleSortChange}>
+          <option value="" selected disabled>
+            Sort by:
+          </option>
+          <option value="descending">Descending</option>
+          <option value="ascending">Ascending</option>
+        </select>
+      </div>
+      <div className={styles.productListingContainer}>
+        {sortedProducts.map((product) => {
+          return (
+            <div key={product.id} className={styles.productListing}>
+              <span>{'£' + product.price}</span>
+              <h3>{product.title}</h3>
+              <div className={styles.image}>
+                <Image src={product.image} alt={product.title} fill />
+              </div>
+              <p>{product.description}</p>
+              <div className={styles.link}>
+                <Link href="">Buy Now</Link>
+              </div>
             </div>
-            <p>{product.description}</p>
-            <div className={styles.link}>
-              <Link href="">Buy Now</Link>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
